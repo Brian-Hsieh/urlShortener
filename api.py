1bb7d44d652d9e7b0f56810086e41f3db2a852ff
Brian <brianhsieh38@gmail.com>
2022-05-22 00:23:57 +0200

    Add namespace to entry point of application

--- api.py
+++ api.py
@@ -1,6 +1,20 @@
 from web import create_app, db
 
+from flask_restx import Api
+from flask import Blueprint
+from web.controller.routes import route as route_ns
+
+blueprint = Blueprint('url', __name__)
+
+url = Api(blueprint,
+          title = 'it is a title',
+          version = '1.0',
+          description = 'url shortener api')
+
+url.add_namespace(route_ns, path = '/api')
+
 app = create_app()
+app.register_blueprint(blueprint)
 
 if __name__ == "__main__":
     db.create_all()
--- web/__init__.py
+++ web/__init__.py
@@ -9,6 +9,4 @@ def create_app():
     app.config.from_pyfile('settings.py')
     db.init_app(app)
     app.app_context().push()
-    from .controller import routes
-    app.register_blueprint(routes, url_prefix = "/")
     return app
